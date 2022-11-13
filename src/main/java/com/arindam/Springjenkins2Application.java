9e01a346da2a7c464a8ead7c948ee18f12817aef
unknown <Arindam Sengupta@ArindamS-GGN.gle.in>
2022-11-13 20:12:30 +0530

    first commit

---
+++ src/main/java/com/arindam/Springjenkins2Application.java
@@ -0,0 +1,13 @@
+package com.arindam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springjenkins2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springjenkins2Application.class, args);
+	}
+
+}
